496eef1a27c8548366c940fc3b93275c3277666a
ltt <877489642@qq.com>
2017-04-18 15:44:13 +0800

    slider

--- js/main.js
+++ js/main.js
@@ -1,27 +1,87 @@
-var sliderWidth = 900;
+var $list = $("#pic_list>li");
+var $dots = $("#dots>li");
+var listArr = [].slice.call($list);
+var len = $list.length;
+var index = 0;
+var activeIndex = 0;
+var preActive = 3;
+prependItem();
 
+tId = setInterval(rightMove,3000);
+// var tId = setInterval(leftMove,3000);
 
-var $sliderObj = $(".slider-contents");
-var $sliderLists =$(".slider-list");
-
-
-
-sliderAuto();
-function sliderAuto() {
-    for (var i = 0 ; i <$sliderLists.length; i++){
-        (function(index){
-            setTimeout(function(){
-                $sliderObj.css({
-                  transform : 'translateX(-' + index*sliderWidth +'px)',
-                  transition: 'transform 0.5s ease-in'
-                });
-            },2000 * index);
-            if(index == 3){
-                $sliderLists[0].insertAfter($sliderLists[3])
-                setTimeout(sliderAuto,2000*(index+1));
-            }
-
-        })(i);
-
+$(window).keydown(function (e) {
+    switch (e.which){
+        case (37):
+            clearInterval(tId);
+            leftMove();
+            tId = setInterval(rightMove,3000);
+            break;
+        case (39):
+            clearInterval(tId);
+            rightMove();
+            tId = setInterval(rightMove,3000);
+            break;
     }
+
+
+
+    
+});
+$("#toggle_box").hover(
+    function () {
+        clearInterval(tId);
+    },
+    function () {
+        tId = setInterval(rightMove,3000);
+    }
+);
+
+function rightMove(){
+    $('#pic_list').animate({left:'-=900px'},300,function(){
+        appendItem();
+        $(this).css('left',-900);
+    });
 }
+
+
+function leftMove(){
+    $('#pic_list').animate({left:'+=900px'},300,function(){
+        prependItem();
+        $(this).css('left',-900);
+        activeIndex -= 1;
+        if(activeIndex < 0){
+            activeIndex = len - 1;
+        }
+
+    });
+}//图片宽度900px
+
+function prependItem() {
+    index = index - 1;
+    activeIndex = index + 1;
+    if(index < 0){
+        index = len - 1;
+    }
+    if(activeIndex == len){
+        activeIndex = 0;
+    }
+    $('#pic_list').prepend(listArr[index]);
+    $dots.eq(activeIndex).removeClass("active");
+    $dots.eq(activeIndex - 1).addClass("active");
+
+
+
+}
+
+function appendItem() {
+    $('#pic_list').append(listArr[index]);
+    index = (index + 1) % 4;
+    activeIndex = index +1 ;
+    if(activeIndex == len){
+        activeIndex = 0;
+    }
+    $dots.eq(index).removeClass("active");
+    $dots.eq(activeIndex).addClass("active");
+}
+
