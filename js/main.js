a8cffd5600cc15df13da46b8dbf2e28cba7ca898
ltt <877489642@qq.com>
2017-04-18 15:52:59 +0800

    slider

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 var $list = $("#pic_list>li");
 var $dots = $("#dots>li");
-var listArr = [].slice.call($list);
+var listArr = [].slice.call($list); //list 的dom数组
 var len = $list.length;
 var index = 0;
 var activeIndex = 0;
-var preActive = 3;
-prependItem();
+var preActive = 0;
+prependItem(); //
 
 tId = setInterval(rightMove,3000);
 // var tId = setInterval(leftMove,3000);
 
+
+// 按下left(37)和right(39)键触发的事件
 $(window).keydown(function (e) {
     switch (e.which){
         case (37):
-            clearInterval(tId);
-            leftMove();
-            tId = setInterval(rightMove,3000);
+            clearInterval(tId); // 清除定时器
+            leftMove(); //向左滑动
+            tId = setInterval(rightMove,3000);//设置定时器
             break;
         case (39):
             clearInterval(tId);
@@ -28,13 +30,15 @@ $(window).keydown(function (e) {
 
     
 });
+
+// slider鼠标滑过的触发事件
 $("#toggle_box").hover(
     function () {
         clearInterval(tId);
-    },
+    }, // mouseover时清除定时器
     function () {
         tId = setInterval(rightMove,3000);
-    }
+    }// mouseout
 );
 
 function rightMove(){
@@ -57,8 +61,9 @@ function leftMove(){
     });
 }//图片宽度900px
 
+//前插item
 function prependItem() {
-    index = index - 1;
+    index = index - 1; // index 渐小
     activeIndex = index + 1;
     if(index < 0){
         index = len - 1;
@@ -67,13 +72,14 @@ function prependItem() {
         activeIndex = 0;
     }
     $('#pic_list').prepend(listArr[index]);
+    // dots 的焦点变化
     $dots.eq(activeIndex).removeClass("active");
     $dots.eq(activeIndex - 1).addClass("active");
 
 
 
 }
-
+//
 function appendItem() {
     $('#pic_list').append(listArr[index]);
     index = (index + 1) % 4;
